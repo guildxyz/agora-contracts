cb86639a16e4f64584dee3ad52f68b2065a5af4b
Tomi_Ohl <ohlslager.tom@gmail.com>
2021-07-27 14:23:54 +0200

    Rename tokens to match their off-chain names

--- migrations/4_deploy_space.js
+++ migrations/4_deploy_space.js
@@ -1,13 +1,13 @@
 // Token properties
 // NOTE: change them before deploying
-const stakeTokenAddress = "INSERT_HERE";
-const returnTokenName = "Agora.space Token";
-const returnTokenSymbol = "AGT";
+const tokenAddress = "INSERT_HERE";
+const stakeTokenName = "Agora.space Token";
+const stakeTokenSymbol = "AGT";
 
 // Contracts
 const read = require("readline-sync");
 const staking = artifacts.require("AgoraSpace");
-const returnToken = artifacts.require("AgoraToken");
+const stakeToken = artifacts.require("AgoraToken");
 
 if (read.keyInYN("WAIT! Did you set the token's properties in this script (4_deploy_space.js)?")) {
   console.log("Cool, let the deployment begin...");
@@ -20,18 +20,17 @@ module.exports = async (deployer) => {
   // Get the decimals of the staked token
   const erc20Abi = require("../build/contracts/ERC20.json").abi;
   const web3 = staking.interfaceAdapter.web3;
-  const tokenContract = new web3.eth.Contract(erc20Abi, stakeTokenAddress);
-  const returnTokenDecimals = await tokenContract.methods.decimals().call();
-  console.log(`The staked token has ${returnTokenDecimals} decimals.`);
+  const tokenContract = new web3.eth.Contract(erc20Abi, tokenAddress);
+  const stakeTokenDecimals = await tokenContract.methods.decimals().call();
+  console.log(`The community's token has ${stakeTokenDecimals} decimals.`);
 
-  // Deploy the returnToken and then the staking contract
-  await deployer.deploy(returnToken, returnTokenName, returnTokenSymbol, returnTokenDecimals);
-  await deployer.deploy(staking, stakeTokenAddress, returnToken.address);
+  // Deploy the stakeToken and then the staking contract
+  await deployer.deploy(stakeToken, stakeTokenName, stakeTokenSymbol, stakeTokenDecimals);
+  await deployer.deploy(staking, tokenAddress, stakeToken.address);
 
   // Transfer ownerShip of the token to the staking contract
   console.log("Transferring the token's ownership to the Agora Space contract...");
-  const tokenInstance = await returnToken.deployed();
-  await tokenInstance.transferOwnership(staking.address);
-  const newOwner = tokenInstance.owner();
-  console.log("Agora Token's ownership successfully transferred.");
+  const stakeTokenInstance = await stakeToken.deployed();
+  await stakeTokenInstance.transferOwnership(staking.address);
+  const newOwner = stakeTokenInstance.owner();
 };
