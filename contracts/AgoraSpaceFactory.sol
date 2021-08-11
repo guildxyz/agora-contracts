a0f09b6232e52160fd57882249d81b592a97a3b6
Tomi_Ohl <ohlslager.tom@gmail.com>
2021-08-11 20:55:06 +0200

    Restrict access to the deployer function Test here: https://testnet.bscscan.com/address/0x8a7e211da40b449be5ba745b6e8dade4c71bf33d

--- contracts/AgoraSpaceFactory.sol
+++ contracts/AgoraSpaceFactory.sol
@@ -6,16 +6,21 @@ import "./token/AgoraToken.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 /// @title A contract that deploys Agora Space contracts for any community
-contract AgoraSpaceFactory {
+contract AgoraSpaceFactory is Ownable {
+    /// @notice EOA => token => approved to deploy?
+    mapping(address => mapping(address => bool)) public approvedAddresses;
+
     /// @notice Token => deployed Space
     mapping(address => address) public spaces;
 
     event SpaceCreated(address token, address space, address agoraToken);
+    event AddressApproved(address account, address token, bool approvalState);
 
     /// @notice Deploys a new Agora Space contract with it's token and registers it in the spaces mapping
     /// @param _token The address of the community's token (that will be deposited to Space)
     function createSpace(address _token) external {
-        require(_token != address(0));
+        require(approvedAddresses[msg.sender][_token], "Sender not authorized");
+        require(_token != address(0), "Token address is zero");
         require(spaces[_token] == address(0), "Space aleady exists");
         string memory tokenSymbol = IERC20Metadata(_token).symbol();
         uint8 tokenDecimals = IERC20Metadata(_token).decimals();
@@ -27,7 +32,20 @@ contract AgoraSpaceFactory {
         AgoraSpace agoraSpace = new AgoraSpace(_token, address(agoraToken));
         spaces[_token] = address(agoraSpace);
         agoraToken.transferOwnership(address(agoraSpace));
-        agoraSpace.transferOwnership(address(msg.sender));
+        agoraSpace.transferOwnership(msg.sender);
         emit SpaceCreated(_token, address(agoraSpace), address(agoraToken));
+    }
+
+    /// @notice Sets the approval state of an address
+    /// @param _tokenOwner The owner of the token authorized on agora.space
+    /// @param _tokenAddress The address of the token whose owner is being approved
+    /// @param _approvalState Whether to approve or disapprove
+    function setApproval(
+        address _tokenOwner,
+        address _tokenAddress,
+        bool _approvalState
+    ) external onlyOwner {
+        approvedAddresses[_tokenOwner][_tokenAddress] = _approvalState;
+        emit AddressApproved(msg.sender, _tokenAddress, _approvalState);
     }
 }
